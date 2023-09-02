a69a428c6dd229aa16970a0404884d3eabb74036
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2023-09-01 22:27:19 -0400

    CSS is clean up, changed file name and media query added

---
+++ README.md
@@ -0,0 +1,83 @@
+````markdown
+# Analog Wall Clock
+
+![Hyper-Realistic SVG Wall Clock](link_to_clock_image.png)
+
+## Overview
+
+This is a hyper-realistic analog clock animated using JavaScript and styled with CSS. It combines the beauty of SVG graphics with the functionality of JavaScript to create an interactive and visually appealing clock.
+
+## Features
+
+- Realistic analog clock design.
+- Smooth and continuous second, minute, and hour hands movement.
+- Interactive and responsive to real-time.
+- Highly customizable through CSS.
+
+## Demo
+
+You can see a live demo of the clock here: [Live Demo](waiz3ple.github.io/wall-clock)
+
+## Getting Started
+
+Follow these instructions to get a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+You will need a web browser that supports SVG and JavaScript.
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/waiz3ple/wall-clock.git
+   ```
+````
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd wall-clock
+   ```
+
+3. Open the `index.html` file in your preferred web browser.
+
+## Usage
+
+- The clock should start running immediately when you open the `index.html` file in your browser.
+- You can interact with the clock in real-time, and the time should update accordingly.
+
+## Built With
+
+- SVG - Scalable Vector Graphics for creating the clock face.
+- JavaScript - For handling the clock's animation and real-time updates.
+- CSS - For styling the clock and making it visually appealing.
+
+## Acknowledgments
+
+- Thanks to [Author Name] for the SVG clock inspiration.
+- [Link to Inspirational Clock Project]
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+please do not claim this project as yours - it is solely design and built by WASIU RAMONI for talent showcase.
+
+## Author
+
+- Wasiu
+- GitHub: [GitHub Profile](https://github.com/waiz3ple)
+
+## Contact
+
+If you have any questions, suggestions, or issues, please feel free to reach out to me:
+
+- Email: wasiu@ramoni.pro
+- Twitter: @waiz3ple
+- Telegram: t.me/waiz3ple
+
+```
+
+
+```
--- script.js
+++ script.js
@@ -4,66 +4,71 @@ const hourHand = document.querySelector('#hourHand');
 const audio = new Audio('./sounds/tick-tock.wav');
 const btnBox = document.querySelector('.switches--container');
 
-function clock(){
-    const time = new Date();
-    const sec = time.getSeconds();
-    const min = time.getMinutes();
-    const hour  = time.getHours() > 12 ? time.getHours() - 12 : time.getHours(); // compress to 12hrs
-    
-   //rotations : I explain this calculations in the comment folder
-    const secR = sec *  6 //  x 360° 
-    const minR = (min * 6) + (sec/10); // 
-    const hrR = (hour * 30) + (min/2) + (sec/120); //  
-   
-   rotate(secHand,  secR);  
-   rotate(minHand,  minR);
-   rotate(hourHand, hrR);
-} 
+function clock() {
+	const time = new Date();
+	const sec = time.getSeconds();
+	const min = time.getMinutes();
+	const hour = time.getHours() > 12 ? time.getHours() - 12 : time.getHours(); // compress to 12hrs
 
-function rotate(hand, degrees){
-   hand.setAttribute('transform', `rotate(${degrees}, -128, -154)`);
+	//rotations : I explain this calculations in the docs folder
+	const secR = sec * 6; //  x 360°
+	const minR = min * 6 + sec / 10; //
+	const hrR = hour * 30 + min / 2 + sec / 120; //
+
+	rotate(secHand, secR);
+	rotate(minHand, minR);
+	rotate(hourHand, hrR);
+}
+
+function rotate(hand, degrees) {
+	hand.setAttribute('transform', `rotate(${degrees}, -128, -154)`);
 }
 clock();
-setInterval(clock, 1000)
-
+setInterval(clock, 1000);
 
 //____________light mode and sound___________
 
 //watching all checkboxes via event delegation
-btnBox.addEventListener('change', function(event){ 
-      //sound switch
-    if(event.target.classList.contains('toggle-sound')){
-           event.target.checked ? playSound() : audio.pause(); //after cooling off
-    }
-     //mode switch
-    if(event.target.classList.contains('toggle-mode')){
-        event.target.checked ? darkMode() : lightMode(); 
-    }
-})
+btnBox.addEventListener('change', function (event) {
+	//sound switch
+	if (event.target.classList.contains('toggle-sound')) {
+		event.target.checked ? playSound() : audio.pause(); //after cooling off
+	}
+	//mode switch
+	if (event.target.classList.contains('toggle-mode')) {
+		event.target.checked ? darkMode() : lightMode();
+	}
+});
 
-
-function playSound(){
-    audio.play();
-    if(typeof audio.loop ==='boolean') audio.loop = true;
+function playSound() {
+	audio.play();
+	if (typeof audio.loop === 'boolean') audio.loop = true;
 }
 
+const classGroup = (selector) =>
+	Array.from(document.querySelectorAll(selector));
 
-const classGroup = (selector) => Array.from(document.querySelectorAll(selector));
-
-function darkMode(){
-    classGroup(`[class^="st"]`).forEach( element => {
-    const lightClass =  element.getAttribute('class').split(' ').find( cl => {
-        if(cl.startsWith('st') && !cl.endsWith('--darkMode')) return cl;
-      });
-      element.classList.toggle(`${lightClass}--darkMode`)
-      element.classList.toggle(`${lightClass}`)
-   });  
+function darkMode() {
+	classGroup(`[class^="st"]`).forEach((element) => {
+		const lightClass = element
+			.getAttribute('class')
+			.split(' ')
+			.find((cl) => {
+				if (cl.startsWith('st') && !cl.endsWith('--darkMode')) return cl;
+			});
+		element.classList.toggle(`${lightClass}--darkMode`);
+		element.classList.toggle(`${lightClass}`);
+	});
 }
 
-function lightMode(){
-   classGroup(`[class$="--darkMode"]`).forEach(element =>{
-    const lightClass = element.getAttribute('class').split(' ').find(cl => cl.endsWith('--darkMode')).replace('--darkMode','')
-       element.classList.toggle(`${lightClass}`)
-       element.classList.toggle(`${lightClass}--darkMode`)
-  });
+function lightMode() {
+	classGroup(`[class$="--darkMode"]`).forEach((element) => {
+		const lightClass = element
+			.getAttribute('class')
+			.split(' ')
+			.find((cl) => cl.endsWith('--darkMode'))
+			.replace('--darkMode', '');
+		element.classList.toggle(`${lightClass}`);
+		element.classList.toggle(`${lightClass}--darkMode`);
+	});
 }
