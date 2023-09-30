e61afe5663415b47558ca34ae1333ae0c9b79aae
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2023-09-29 20:49:38 -0400

    refactor all

--- script.js
+++ script.js
@@ -1,74 +1,56 @@
+/*programming paradigm: functional*/
+
 const secHand = document.querySelector('#secHand');
 const minHand = document.querySelector('#minHand');
 const hourHand = document.querySelector('#hourHand');
 const audio = new Audio('./sounds/tick-tock.wav');
-const btnBox = document.querySelector('.switches--container');
+const btnBox = document.querySelector('.switches-container');
+const coords = [-128, -154]; //from the svg center point
 
 function clock() {
-	const time = new Date();
-	const sec = time.getSeconds();
-	const min = time.getMinutes();
-	const hour = time.getHours() > 12 ? time.getHours() - 12 : time.getHours(); // compress to 12hrs
+    const time = new Date();
+    const sec  =  time.getSeconds();
+    const min  =  time.getMinutes();
+    const hour = time.getHours() > 12 ? time.getHours() - 12 : time.getHours(); // compress to 12hrs
 
-	//rotations : I explain this calculations in the docs folder
-	const secR = sec * 6; //  x 360°
-	const minR = min * 6 + sec / 10; //
-	const hrR = hour * 30 + min / 2 + sec / 120; //
+    //Calculations: check ==> https://github.com/waiz3ple/wall-clock/blob/master/docs/Clock%20Hands.pdf
+    const secR = sec * 6; 
+    const minR = min * 6 + sec / 10; //
+    const hrR = hour * 30 + min / 2 + sec / 120; //
 
-	rotate(secHand, secR);
-	rotate(minHand, minR);
-	rotate(hourHand, hrR);
+    rotate(secHand, secR);
+    rotate(minHand, minR);
+    rotate(hourHand, hrR);
 }
 
 function rotate(hand, degrees) {
-	hand.setAttribute('transform', `rotate(${degrees}, -128, -154)`);
+    const [xAxis, yAxis] = coords
+    hand.setAttribute('transform', `rotate(${degrees}, ${xAxis}, ${yAxis})`);
 }
+
 clock();
 setInterval(clock, 1000);
 
 //____________light mode and sound___________
 
-//watching all checkboxes via event delegation
+//event delegation 
 btnBox.addEventListener('change', function (event) {
-	//sound switch
-	if (event.target.classList.contains('toggle-sound')) {
-		event.target.checked ? playSound() : audio.pause(); //after cooling off
-	}
-	//mode switch
-	if (event.target.classList.contains('toggle-mode')) {
-		event.target.checked ? darkMode() : lightMode();
-	}
+    //sound switch
+    if (event.target.classList.contains('toggle-sound')) {
+        event.target.checked ? playSound() : audio.pause(); 
+    }
+    //switch color scheme
+    if (event.target.classList.contains('toggle-color')) {
+        event.target.checked ? applyColorScheme('dark'): applyColorScheme('light');
+    }
 });
 
 function playSound() {
-	audio.play();
-	if (typeof audio.loop === 'boolean') audio.loop = true;
+    audio.play();
+    if (typeof audio.loop === 'boolean') audio.loop = true;
 }
 
-const classGroup = (selector) =>
-	Array.from(document.querySelectorAll(selector));
-
-function darkMode() {
-	classGroup(`[class^="st"]`).forEach((element) => {
-		const lightClass = element
-			.getAttribute('class')
-			.split(' ')
-			.find((cl) => {
-				if (cl.startsWith('st') && !cl.endsWith('--darkMode')) return cl;
-			});
-		element.classList.toggle(`${lightClass}--darkMode`);
-		element.classList.toggle(`${lightClass}`);
-	});
-}
-
-function lightMode() {
-	classGroup(`[class$="--darkMode"]`).forEach((element) => {
-		const lightClass = element
-			.getAttribute('class')
-			.split(' ')
-			.find((cl) => cl.endsWith('--darkMode'))
-			.replace('--darkMode', '');
-		element.classList.toggle(`${lightClass}`);
-		element.classList.toggle(`${lightClass}--darkMode`);
-	});
-}
+function applyColorScheme(scheme='dark'){
+    const root = document.documentElement;
+    root.setAttribute('data-theme', scheme)
+ }
