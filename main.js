eecea1ce66db47788e80e495255c8be87ca2065b
Wasiu Ramoni <waiz3ple@gmail.com>
2024-07-31 20:44:51 -0400

    main.js file is added to upgrade this code in OOP

---
+++ main.js
@@ -0,0 +1,50 @@
+// this is OOP version of script.js
+class Clock {
+	#secondsDeg;
+	#minutesDeg;
+	#hoursDeg;
+	#secHand;
+	#minHand;
+	#hourHand;
+	#coordinate = { x: 0.5, y: 3.5 };
+	
+	static select(selector) {
+		return document.querySelector(selector);
+	}
+
+	constructor() {
+		this.#secHand  = Clock.select('#secHand');
+		this.#minHand  = Clock.select('#minHand');
+		this.#hourHand = Clock.select('#hourHand');
+		this.rotateHands();
+	}
+
+	#updateTime() {
+		const time = new Date();
+		const seconds = time.getSeconds();
+		const minutes = time.getMinutes();
+		const hours   = time.getHours() % 12 || 12;
+
+		// Angle of rotation in degree
+		this.#secondsDeg = seconds * 6;
+		this.#minutesDeg = minutes * 6 + seconds / 10;
+		this.#hoursDeg = hours * 30 + minutes / 2 + seconds / 120;
+	}
+
+	#handRotation(hand, angle) {
+		const { x, y } = this.#coordinate;
+		hand.setAttribute('transform', `rotate(${angle}, ${x}, ${y})`);
+	}
+
+	 rotateHands() {
+		this.#updateTime();
+		this.#handRotation(this.#secHand, this.#secondsDeg);
+		this.#handRotation(this.#minHand, this.#minutesDeg);
+		this.#handRotation(this.#hourHand, this.#hoursDeg);
+	}
+}
+ 
+
+const clock = new Clock();
+
+setInterval(() => clock.rotateHands(), 1000);
