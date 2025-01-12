6ed61d083a115ba96c9eb7443bb05e4f21c44246
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-01-12 15:41:39 -0500

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,5 @@ export default defineConfig({
     build: {
         target: "esnext",
         polyfillDynamicImport: false,
-    },
-    esbuild: {
-        jsxFactory: "h",
-        jsxFragment: "Fragment",
-    },
-    server: {
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-        "/@/": new URL("./src", import.meta.url).pathname,
-        },
-    },
+    }
 });
